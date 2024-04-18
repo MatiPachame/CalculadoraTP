c38567b6e85687e6bc6d66d17ae447bc9637b418
MatiPachame <matipachame16@gmail.com>
2024-04-18 10:30:03 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { CalculadoraComponent } from './componentes/calculadora/calculadora.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [path:"**",component:CalculadoraComponent];
---
+++ src/app/clases/sumadora.ts
@@ -0,0 +1,59 @@
+export class Sumadora {
+
+    public numero1:number;
+    public numero2: number;
+    public operacion: number = 0;
+    public resultado: number;
+
+
+    constructor(){
+        this.numero1=0;
+        this.numero2=0;
+        this.resultado=0;
+    }
+
+    public calcular(){
+        this.operacion= parseInt(this.operacion.toString());
+        switch(this.operacion){
+            case 1:
+                this.resultado = this.numero1 + this.numero2;
+                break;
+
+            case 2:
+                this.resultado = this.numero1 - this.numero2;
+                break;
+            
+            case 3:
+                this.resultado = this.numero1 / this.numero2;
+                break;
+
+            case 4:
+                this.resultado = this.numero1 * this.numero2;
+                break;
+
+            default:
+                this.resultado = 0;
+        }
+
+        this.numero2 = this.resultado;
+        this.numero1 = 0;
+    }
+
+    public concatenar(valor :number){
+        this.numero2 = this.numero2 * 10 + parseInt(valor.toString());
+    }
+
+    public limpiar(){
+        this.numero2 = 0;
+    }
+
+    public realizarOperacion(operacion: number){
+
+        if(this.numero1!=0) this.calcular();
+        this.operacion = parseInt(operacion.toString());
+        this.numero1= this.numero2;
+        this.limpiar;
+
+    }
+
+}
